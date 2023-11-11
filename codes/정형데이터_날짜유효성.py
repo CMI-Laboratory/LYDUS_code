4aca81e514a55aac6fe8c813ee8f6a0054eef88f
Yujin0515 <126840734+Yujin0515@users.noreply.github.com>
2023-11-11 17:34:50 +0900

    rename

--- codes/정형데이터_날짜유효성.py
+++ codes/정형데이터_날짜유효성.py
@@ -3,14 +3,13 @@
 
 # In[ ]:
 
-
 get_ipython().system('pip install openai')
 get_ipython().system('pip install langchain')
 
 import os
 
 #@markdown https://platform.openai.com/account/api-keys
-OPENAI_API_KEY = "sk" #@param {type:"string"}
+OPENAI_API_KEY = "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" #@param {type:"string"}
 os.environ["OPENAI_API_KEY"] = OPENAI_API_KEY
 
 import pandas as pd
@@ -42,13 +41,13 @@ def valid_date_custom(date_string, formats):
     return False
 
 def validate_date_entry(df, date_string, date_col_name):
-    #date_col_name = '기록날짜'
     if pd.isna(date_string):
         return
-
+    #유효한 날짜 필터링
     if is_valid_date(date_string) or valid_date_custom(date_string, date_formats):
         #print(f"{date_string}은(는) 유효한 날짜입니다.")
         df.loc[df[date_col_name] == date_string, 'date_valid'] = 0
+    #유효하지 않은 날짜 GPT에 검증
     else:
         #print(f"{date_string}이(가) 유효한 날짜인지 검증하겠습니다.")
         user_response = llm(f"Is {date_string} a valid date? Please answer with 'yes' or 'no'.").strip()
@@ -56,8 +55,8 @@ def validate_date_entry(df, date_string, date_col_name):
             df.loc[df[date_col_name] == date_string, 'date_valid'] = 1
             #print(f"{date_string}은(는) 유효하지 않은 날짜입니다.")
         else:
-            #print(f"{date_string}은(는) 유효한 날짜입니다.")
             df.loc[df[date_col_name] == date_string, 'date_valid'] = 0
+            #print(f"{date_string}은(는) 유효한 날짜입니다.")
 
 def validate_dates(csv_file_name):
     df = pd.read_csv(csv_file_name)
@@ -70,6 +69,14 @@ def validate_dates(csv_file_name):
         validate_date_entry(df, date_string, date_col_name)
     
     valid_entries = df[df['date_valid'] != -1]
-    percentage = (valid_entries['date_valid'] == 0).mean() 
-    print("날짜 유효성:",percentage)
+    date_validation = (valid_entries['date_valid'] == 0).mean() 
+    date_validation = round(date_validation, 2)
+    print("날짜 유효성:",date_validation)
+    
+    # 유효하지 않은 날짜들을 CSV 파일로 저장
+    invalid_dates_df = df[df['date_valid'] == 1]
+    invalid_dates_df = invalid_dates_df.drop('date_valid', axis=1)
+    invalid_dates_df.to_csv('invalid_dates.csv', index=False)
+    
+    
 
